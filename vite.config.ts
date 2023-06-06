13da6193e5492d9f029a52ebecd472e1687d15e2
th-m <thomvaladez@gmail.com>
2023-06-06 15:13:54 -0600

    see if deploy works

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-
+// @ts-ignore
+import netlify from "solid-start-netlify";
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({ adapter: netlify() })],
 });
